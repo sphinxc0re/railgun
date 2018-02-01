e17f7ded4e51645385b5d08125487266f49bbfb7
Julian Laubstein <contact@julianlaubstein.de>
2018-02-01 19:20:05 +0100

    Adjusted grammar + added two failign tests

--- core/parser/mod.rs
+++ core/parser/mod.rs
@@ -12,32 +12,14 @@ mod tests {
 
     #[test]
     fn simple_parser() {
-        let input = r#"
-            (module
-                (func (export "unaligned") (result i32)
-                    (local i32 f64 f64)
-                    (set_local 0 (i32.const 10))
-                      (block
-                        (loop
-                          (if
-                            (i32.eq (get_local 0) (i32.const 0))
-                            (then (br 2))
-                          )
-                          (set_local 2 (f64.convert_s/i32 (get_local 0)))
-                          (f64.store align=1 (get_local 0) (get_local 2))
-                          (set_local 1 (f64.load align=1 (get_local 0)))
-                          (if
-                            (f64.ne (get_local 2) (get_local 1))
-                            (then (return (i32.const 0)))
-                          )
-                          (set_local 0 (i32.sub (get_local 0) (i32.const 1)))
-                          (br 0)
-                        )
-                      )
-                      (i32.const 1)
-                )
-            )
-        "#;
+        let input = include_str!("testfiles/test1.wast");
+
+        WastParser::parse(Rule::file, input.trim()).unwrap_or_else(|e| panic!("{}", e));
+    }
+
+    #[test]
+    fn fission_test() {
+        let input = include_str!("testfiles/fission.wast");
 
         WastParser::parse(Rule::file, input.trim()).unwrap_or_else(|e| panic!("{}", e));
     }
---
+++ core/parser/testfiles/test1.wast
@@ -0,0 +1,4 @@
+(module
+  (global $test i32)
+  (memory 32 512)
+  (start 0))
